5c8a7af620fb4e500733ca6919d1b11947986a7b
pdlon <pdlong578@gmail.com>
2025-09-03 20:30:24 +0700

    api demo

---
+++ src/TodoApp.HttpApi/Controllers/DemoLocalizationController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace TodoApp.Controllers
+{
+    [Route("api/localization-demo")]
+    public class DemoLocalizationController : AbpController
+    {
+        private readonly IStringLocalizer<DemoLocalizationController> _localizer;
+
+        public DemoLocalizationController(IStringLocalizer<DemoLocalizationController> localizer)
+        {
+            _localizer = localizer;
+        }
+
+        [HttpGet("hello")]
+        public string GetHello()
+        {
+            // Key "Hello" sẽ được dịch theo ngôn ngữ hiện tại
+            return _localizer["Hello"];
+        }
+    }
+}
--- src/TodoApp.HttpApi/Controllers/TodoAppController.cs
+++ src/TodoApp.HttpApi/Controllers/TodoAppController.cs
@@ -1,14 +1,23 @@
-﻿using TodoApp.Localization;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using TodoApp.Localization;
 using Volo.Abp.AspNetCore.Mvc;
 
 namespace TodoApp.Controllers;
 
 /* Inherit your controllers from this class.
  */
+[Microsoft.AspNetCore.Components.Route("api/localization-demo")]
 public abstract class TodoAppController : AbpControllerBase
 {
     protected TodoAppController()
     {
         LocalizationResource = typeof(TodoAppResource);
+    }
+    [HttpGet("hello")]
+    public string GetHello()
+    {
+        // Key "Hello" sẽ được dịch theo ngôn ngữ hiện tại
+        return "hello";
     }
 }
